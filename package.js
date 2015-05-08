ef3ac770ca561e663f9d0a10c1c9a98a4272b50f
Daniel C <333222@gmail.com>
2015-05-08 11:49:40 +0800

    v0.2, require node's Util on server side

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'meteorspark:util',
   summary: 'Bring some of NodeJS util functionality to the Meteor client',
-  version: '0.1.0',
+  version: '0.2.0',
   git: 'https://github.com/MeteorSpark/util'
 });
 
@@ -12,7 +12,8 @@ client = 'client';
 Package.onUse(function (api) {
   api.versionsFrom('0.9.4');
 
-  api.add_files('lib/util.js', client);
+  api.add_files('lib/util-client.js', client);
+  api.add_files('lib/util-server.js', server);
 
   api.export('Util');
 });
